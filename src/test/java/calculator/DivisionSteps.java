b77a0068b4db2eda10b04a575db8ff4ab400ad1f
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2024-02-26 14:56:25 +0100

    redefine division feature's scenarios and define division feature's steps

---
+++ src/test/java/calculator/DivisionSteps.java
@@ -0,0 +1,50 @@
+package calculator;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DivisionSteps {
+
+    private Calculator calculator;
+    private ArithmeticException exception;
+
+    @Given("I have a calculator")
+    public void IHaveACalculator(){
+        this.calculator = new Calculator();
+    }
+
+    @When("I enter {int} and {int}")
+    public void IEnterTwoNumbers(int arg0, int arg1){
+        this.calculator.enter(arg0);
+        this.calculator.enter(arg1);
+    }
+
+    @When("I press the division button")
+    public void IPressDivision(){
+        try {
+            this.calculator.divide();
+        }catch (ArithmeticException ex){
+            this.exception = ex;
+        }
+    }
+
+    @Then("the result should be {int}")
+    public void TheResultShouldBe(int res){
+        assertEquals(res, this.calculator.getResult());
+    }
+
+    @When("I enter one number and zero")
+    public void IEnterOneNumberAndZero(int arg0){
+        this.calculator.enter(arg0);
+        this.calculator.enter(0);
+    }
+
+    @Then("the Calculator must warn me that division by zero is not possible")
+    public void IHaveDivisionByZeroError(){
+        assertEquals(exception, new ArithmeticException("Division by zero is not possible"));
+    }
+
+}
--- src/test/resources/features/Division.feature
+++ src/test/resources/features/Division.feature
@@ -1,16 +1,18 @@
-Feature: I want to be able to divide two numbers using a Calculator
+Feature: Division of two integer numbers
 
   Background: Start with a Calculator
     Given I have a Calculator
 
   Scenario Outline: Divide one number by another number that is not zero
-    When I divide <Arg0> by <Arg1>
-    Then the result is <Res>
+    When I enter <Arg0> and <Arg1>
+    And I press the division button
+    Then the result should be <Res>
     Examples:
     | Arg0 | Arg1 | Res |
     |  6   |  3   |  2  |
     |  0   |  3   |  0  |
 
   Scenario: Divide one number by zero
-    When I divide any number by zero
+    When I enter one number and zero
+    And I press the division button
     Then the Calculator must warn me that division by zero is not possible
